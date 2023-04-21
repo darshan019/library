5e6961d8b7915b91bfe9bd7ea6928a43b6b605b3
fenrir <darsha019@gmail.com>
2023-04-21 22:29:52 +0530

    project completed the  working is fine

---
+++ script.js
@@ -0,0 +1,77 @@
+let myLibrary = []
+document.getElementById('submit').addEventListener('click', addBookToLibrary)
+
+function Book(name, author, pages, read) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(event) {
+    event.preventDefault()
+    let book_name = document.getElementById('book-name').value
+    let book_author = document.getElementById('author').value
+    let book_pages = document.getElementById('pages').value
+    let readCheck;
+
+    if(document.getElementById('checkbox').checked === true){
+        readCheck = 'Yes'
+    }
+    else{
+        readCheck = 'No'
+    }
+
+    const bookAdd = new Book(book_name, book_author, book_pages, readCheck)
+    myLibrary.push(bookAdd)
+
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(i === myLibrary.length - 1) {
+            bookDisplay(myLibrary[myLibrary.length-1])
+        }
+        else{
+            continue
+        }
+        
+    }
+}
+
+function bookDisplay(item){
+    let bookCards = document.querySelector('.book-cards')
+
+    const div = document.createElement('div')
+    div.classList.add('card')
+
+    let nameBook = document.createElement('span')
+    nameBook.textContent = 'Name: ' + item.name
+    div.appendChild(nameBook)
+
+    let authorBook = document.createElement('span')
+    authorBook.textContent += 'Author: ' + item.author
+    div.appendChild(authorBook)
+
+    let pagesOfBook = document.createElement('span')
+    pagesOfBook.textContent = 'Pages: ' + item.pages
+    div.appendChild(pagesOfBook)
+
+    let read = document.createElement('span')
+    read.textContent = 'Read: ' + item.read
+
+    div.appendChild(read)
+
+    bookCards.appendChild(div)
+
+    book_name = document.getElementById('book-name').value = ''
+    book_author = document.getElementById('author').value = ''
+    book_pages = document.getElementById('pages').value = ''
+
+    let rmbtn = document.createElement('button')
+    rmbtn.textContent = 'Delete'
+    rmbtn.addEventListener('click',() => {
+        div.setAttribute('id', `new_${myLibrary.length-1}`)
+        document.getElementById(`new_${myLibrary.length-1}`).remove()
+        myLibrary.splice(myLibrary.indexOf(item),1)
+    })
+    div.appendChild(rmbtn)
+
+}
